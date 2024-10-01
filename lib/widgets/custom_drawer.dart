2d345d970f3f5c1ef3d234bd7b0df9b16ebb58a2
AmerShammout <amershammout2004@gmail.com>
2024-10-01 19:50:41 +0300

    custom drawer done

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/widgets/drawer_items_list_view.dart';
+import 'package:responsive_dash_board/widgets/in_active_drawer_item.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +23,24 @@ class CustomDrawer extends StatelessWidget {
             height: 8,
           ),
           DrawerItemsListView(),
+          Spacer(),
+          InActiveDrawerItem(
+            drawerItem: DrawerItemModel(
+              title: "Setting system",
+              image: Assets.imagesSetting,
+            ),
+          ),
+          InActiveDrawerItem(
+            drawerItem: DrawerItemModel(
+              title: "Logout account",
+              image: Assets.imagesLogout,
+            ),
+          ),
+          SizedBox(
+            height: 48,
+          ),
         ],
       ),
     );
   }
 }
-
