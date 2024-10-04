a4b42824a2f2d4c1a0ba4641cbb0812547e9c6b4
AmerShammout <amershammout2004@gmail.com>
2024-10-04 13:33:13 +0300

    my card done

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expanses.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/my_card.dart';
 import 'package:responsive_dash_board/widgets/quick_invoice.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -21,9 +22,10 @@ class DashboardDesktopLayout extends StatelessWidget {
           flex: 2,
           child: AllExpensesAndQuickInvoiceSection(),
         ),
-        // SizedBox(
-        //   width: 24,
-        // ),
+         SizedBox(
+          width: 24,
+        ),
+        Expanded(child: MyCard())
       ],
     );
   }
